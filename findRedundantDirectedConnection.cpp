cb765feb9a0776df80a94546fe2f048bfdfce0c1
Paradox <122047216+mukanjy0@users.noreply.github.com>
2024-02-14 12:33:12 -0500

    added solution to problem 3

--- findRedundantDirectedConnection.cpp
+++ findRedundantDirectedConnection.cpp
@@ -1,48 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define sz(x) (int)x.size()
 
 class Solution {
 public:
-    int find(vector<int> &root, int i){
-        return root[i] == i ? i : find(root, root[i]);
-    }
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        int n = sz(edges);
+        vector<int> adj[n+1];
+        for (const auto& edge : edges) {
+            int u1 = edge[0], v1 = edge[1];
+            adj[u1].push_back(v1);
 
-        int n = edges.size();
-        vector<int> parent(n+1, 0), candA, candB;
-        vector<int> root(n+1, 0);
-        for(auto &e: edges){
-            if(parent[e[1]] == 0){
-                parent[e[1]] = e[0];
-            }else{
-                candA = {parent[e[1]], e[1]};
-                candB = e;
-                e[1] = 0;
+            stack<int> s;
+            vector<int> vis (n+1);
+            bool cycle = false;
+            for (int i = 1; i <= n; ++i) {
+                if (vis[i]) continue;
+                s.push(i);
+                while (!s.empty() && !cycle) {
+                    int u = s.top();
+                    s.pop();
+                    if (vis[u]) { continue; }
+
+                    vis[u] = true;
+                    for (int v : adj[u]) {
+                        if (!vis[v]) s.push(v);
+                        else {
+                            cycle = true;
+                            break;
+                        }
+                    }
+                }
+                if (cycle) return {u1, v1};
             }
         }
-        for(int i = 1; i <= n; i++){
-            root[i] = i;
-        }
-        for(auto &e: edges){
-            if(e[1] == 0) continue;
-            int u = e[0], v = e[1], pu = find(root, u);
-            if(pu == v){
-                if(candA.empty()) return e;
-                return candA;
-            }
-            root[v] = pu;
-        }
-        return candB;
+        return {};
     }
 };
 
-int main(){
-    Solution s;
-    vector<vector<int>> edges = {{1,2}, {2,3}, {3,4}, {4,1},{1,5}};
-    vector<int> res = s.findRedundantDirectedConnection(edges);
-    for(auto &i: res){
-        cout << i << " ";
-    }
-    cout << endl;
+void test() {
+    vector<vector<int>> edges = {{1,2},{1,3},{2,3}};
+    vector<int> ans = {2, 3};
+//    for (auto& el : Solution().findRedundantDirectedConnection(edges))
+//        cout << el << ' ';
+//    cout << '\n';
+    assert(Solution().findRedundantDirectedConnection(edges) == ans);
+
+    edges = {{1,2},{2,3},{3, 4}, {4, 1}, {1, 5}};
+    ans = {4, 1};
+    assert(Solution().findRedundantDirectedConnection(edges) == ans);
+}
+
+int main() {
+    test();
     return 0;
-};
+}
